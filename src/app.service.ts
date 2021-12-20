a19cbc9f888e7ddd9d18744074115c81f4f33715
TamakMoy <tamak.wy.cn@gmail.com>
2021-12-20 16:38:14 +0800

    test mac

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'project init success';
+    return 'project init success in mac';
   }
 }
